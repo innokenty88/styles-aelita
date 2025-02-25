af29d1eb66e192ef092a03261d5b488527214b0d
innokenty88 <106551400+innokenty88@users.noreply.github.com>
2025-02-25 22:00:40 +0300

    Update script-main.js

--- script-main.js
+++ script-main.js
@@ -20,19 +20,11 @@ document.addEventListener("DOMContentLoaded", function () {
             menu.style.bottom = "0"; // Прижимаем к низу
             menu.style.height = `${windowHeight}px`; // Высота меню на всю высоту окна
 
-            // Меняем padding для элементов меню
-            menuItems.forEach(item => {
-                item.style.padding = "3.9vh 2.5vh"; // Меняем padding для активного состояния
-            });
         } else {
             menu.style.position = "absolute"; // Возвращаемся к абсолютному позиционированию
             menu.style.top = `${headerHeight}px`; // Устанавливаем под шапкой
             menu.style.height = `calc(100vh - ${headerHeight}px)`; // Высота меню с учётом шапки
 
-            // Меняем padding для элементов меню
-            menuItems.forEach(item => {
-                item.style.padding = "3.3vh 2vh"; // Меняем padding для неактивного состояния
-            });
         }
     }
 
